fe60c672e0ef5cfbd92a94632b1a4c092f910ce7
oxy-macmini <oxyroid@outlook.com>
2025-04-06 18:20:31 +0800

    build: upgrade 1.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,7 @@ afterEvaluate {
             create<MavenPublication>("maven") {
                 groupId = "com.m3u.extension"
                 artifactId = "api"
-                version = "1.1"
+                version = "1.2"
 
                 from(components["release"])
             }
